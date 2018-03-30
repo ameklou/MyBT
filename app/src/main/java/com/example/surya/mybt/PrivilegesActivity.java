f425ac0491c8d1e9918dcbcfe5d6899c89adfdac
Surya <amekley@gmail.com>
2018-03-30 19:53:22 +0000

    first push

---
+++ app/src/main/java/com/example/surya/mybt/PrivilegesActivity.java
@@ -0,0 +1,13 @@
+package com.example.surya.mybt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrivilegesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privileges);
+    }
+}
